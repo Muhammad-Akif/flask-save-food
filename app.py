11297787afe1f189e74d8121db0b12e730f3cad3
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-04-09 22:59:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -152,10 +152,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
